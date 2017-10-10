f5af5e1b46e2fc8dbc2f03d8800a2e1a8b93767c
YmAngie <a.ymydykova@gmail.com>
2017-10-10 16:45:02 +0300

    Change weather conditions mark-up and styles. Form dictionary & add setInterval().

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-function showConditions() {
+function prepareResultDictionary() {
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?id=484907&APPID=95ccb29f3ee760c73717ed3ff8d3f762", true);
+	xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?id=484907&units=metric&APPID=95ccb29f3ee760c73717ed3ff8d3f762", true);
 	xhr.send();
 
 	xhr.onreadystatechange = function() {
@@ -9,16 +9,47 @@ function showConditions() {
 	  if (xhr.status != 200) {
 	    alert(xhr.status + ': ' + xhr.statusText);
 	  } else {
-	    alert(xhr.responseText);
-		
 		var responseText = xhr.responseText;
-
-		var parsedText = JSON.parse(responseText);
-
-		document.getElementById('result').innerHTML = parsedText.dt;
-
+		var weatherDictionary = JSON.parse(responseText);
+		var resultDictionary = createDictionary(weatherDictionary);
+		updateUI(resultDictionary);
 	  }
-
 	}
-
 }
+
+function updateUI(weather) {
+	document.getElementById("temp").innerHTML = weather["temp"] + "&#176";
+	document.getElementById("wind").innerHTML = weather["speed"] + " m/s";
+	document.getElementById("description").innerHTML = weather["description"];
+	document.getElementById("humidity").innerHTML = weather["humidity"] + "%";
+}
+
+function prepareInitialDictionary() {
+	var initialDictionary = createDictionary(null);
+	updateUI(initialDictionary);
+}
+
+function createDictionary(weatherDictionary) {
+	var resultDictionary = {};
+	if(weatherDictionary) {
+		resultDictionary["description"] = weatherDictionary["weather"][0]["description"];
+		resultDictionary["temp"] = weatherDictionary["main"]["temp"];
+		resultDictionary["speed"] = weatherDictionary["wind"]["speed"];
+		resultDictionary["humidity"] = weatherDictionary["main"]["humidity"];
+	} else {
+		resultDictionary["description"] = "-";
+		resultDictionary["temp"] = "-";
+		resultDictionary["speed"] = "-";
+		resultDictionary["humidity"] = "-";
+	}
+	return resultDictionary;
+}
+
+var timerId = setInterval(function() {
+	prepareResultDictionary()
+}, 60000);
+
+setTimeout(function() {
+  clearInterval(timerId);
+  alert("stop");
+}, 120000);
