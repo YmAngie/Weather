b74c0dfad26895235dccb013bc41837a52c6588d
YmAngie <a.ymydykova@gmail.com>
2017-10-21 20:07:42 +0300

    Setup gulp, sass, autoprefixer, linter and browsersync according Web Tooling & Automation course. Change js for further improvement

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var eslint = require('gulp-eslint');
+
+gulp.task('livereload', () => {
+    browserSync.create();
+
+    browserSync.init({
+		server: "./dist"
+	});
+});
+
+gulp.task('lint', () => {
+    return gulp.src(['**/*.js', '!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        //.pipe(eslint.failAfterError());
+});
+
+gulp.task('copy-html', function() {
+	gulp.src('index.html')
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('copy-images', function() {
+	gulp.src('img/*')
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('styles', function() {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('watch', () => {
+    gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/**/*.js', ['lint']);
+	gulp.watch('index.html', ['copy-html']);
+	gulp.watch('./dist/index.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint', 'livereload', 'watch']);
