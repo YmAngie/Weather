1d9aff4bb5cf9ab556fcd4c32672af269186cf43
YmAngie <a.ymydykova@gmail.com>
2017-10-22 14:15:46 +0300

    Add source maps, image optimization. Chage preprocessor to less.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+//var sass = require('gulp-sass');
+var less = require('gulp-less');
+var path = require('path');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
 var eslint = require('gulp-eslint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin'); // lossless - reducing the file size but not throwing away any information
+var pngquant = require('imagemin-pngquant') // lossy - really small file sizes at the expense of image quality
 
 gulp.task('livereload', () => {
     browserSync.create();
@@ -16,14 +21,18 @@ gulp.task('livereload', () => {
 
 gulp.task('scripts', function() {
 	gulp.src('js/**/*.js')
+		.pipe(sourcemaps.init())
 		.pipe(concat('all.js'))
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('scripts-dist', function() {
 	gulp.src('js/**/*.js')
+		.pipe(sourcemaps.init())
 		.pipe(concat('all.js'))
 		.pipe(uglify())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist/js'));
 });
 
@@ -42,14 +51,28 @@ gulp.task('copy-html', function() {
 
 gulp.task('copy-images', function() {
 	gulp.src('img/*')
+		.pipe(imagemin({
+			progressive: true, 
+			use: [pngquant()]
+		}))
 		.pipe(gulp.dest('dist/img'));
 });
 
+// gulp.task('styles', function() {
+// 	gulp.src('sass/**/*.scss')
+// 		.pipe(sass({
+// 			outputStyle: 'compressed'
+// 		}).on('error', sass.logError))
+// 		.pipe(autoprefixer({
+// 			browsers: ['last 2 versions']
+// 		}))
+// 		.pipe(gulp.dest('dist/css'))
+// 		.pipe(browserSync.stream());
+// });
+
 gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass({
-			outputStyle: 'compressed'
-		}).on('error', sass.logError))
+	gulp.src('less/**/*.less')
+		.pipe(less())
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		}))
@@ -58,11 +81,11 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', () => {
-    gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch('less/**/*.less', ['styles']);
 	gulp.watch('js/**/*.js', ['lint']);
 	gulp.watch('index.html', ['copy-html']);
 	gulp.watch('./dist/index.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint', 'livereload', 'watch']);
+gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint', 'livereload', 'watch', 'scripts']);
 gulp.task('dist', ['copy-html', 'copy-images', 'styles', 'lint', 'scripts-dist']);
